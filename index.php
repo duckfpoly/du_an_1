14903b14139daa4f7ff5c3262bdfa9aa1d13848a
Trung Duc <huyga612@gmail.com>
2022-11-05 15:21:13 +0700

    add home page

--- index.php
+++ index.php
@@ -1,2 +1,96 @@
-<h1>Code đê, nghịc vừa thôi=))</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> App landing</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/img/img_site/img/favicon.ico">
+
+    <link rel="stylesheet" href="./assets/icons/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="./assets/css/css_site/course.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/css_site/course_responsive.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/css_site/animate.css">
+
+
+    <link rel="stylesheet" href="./assets/css/css_site/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/css_site/owl.carousel.min.css">
+    <link rel="stylesheet" href="./assets/css/css_site/slicknav.css">
+    <link rel="stylesheet" href="./assets/css/css_site/flaticon.css">
+    <link rel="stylesheet" href="./assets/css/css_site/progressbar_barfiller.css">
+    <link rel="stylesheet" href="./assets/css/css_site/gijgo.css">
+    <link rel="stylesheet" href="./assets/css/css_site/animate.min.css">
+    <link rel="stylesheet" href="./assets/css/css_site/animated-headline.css">
+    <link rel="stylesheet" href="./assets/css/css_site/magnific-popup.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./assets/css/css_site/themify-icons.css">
+    <link rel="stylesheet" href="./assets/css/css_site/slick.css">
+    <link rel="stylesheet" href="./assets/css/css_site/nice-select.css">
+    <link rel="stylesheet" href="./assets/css/css_site/style.css">
+</head>
+<body>
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/img_site/img/logo/loder.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'views/site/layout/navbar.php'?>
+    <?php include 'views/site/home.php'?>
+    <?php include 'views/site/layout/footer.php'?>
+
+
+<script src="./assets/js/course/jquery-3.2.1.min.js"></script>
+<script src="./assets/js/course/course.js"></script>
+
+<!-- JS here -->
+<script src="./assets/js/js/vendor/modernizr-3.5.0.min.js"></script>
+<!-- Jquery, Popper, Bootstrap -->
+<script src="./assets/js/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="./assets/js/js/popper.min.js"></script>
+<script src="./assets/js/js/bootstrap.min.js"></script>
+<!-- Jquery Mobile Menu -->
+<script src="./assets/js/js/jquery.slicknav.min.js"></script>
+
+<!-- Jquery Slick , Owl-Carousel Plugins -->
+<script src="./assets/js/js/owl.carousel.min.js"></script>
+<script src="./assets/js/js/slick.min.js"></script>
+<!-- One Page, Animated-HeadLin -->
+<script src="./assets/js/js/wow.min.js"></script>
+<script src="./assets/js/js/animated.headline.js"></script>
+<script src="./assets/js/js/jquery.magnific-popup.js"></script>
+
+<!-- Date Picker -->
+<script src="./assets/js/js/gijgo.min.js"></script>
+<!-- Nice-select, sticky -->
+<script src="./assets/js/js/jquery.nice-select.min.js"></script>
+<script src="./assets/js/js/jquery.sticky.js"></script>
+<!-- Progress -->
+<script src="./assets/js/js/jquery.barfiller.js"></script>
+
+<!-- counter , waypoint,Hover Direction -->
+<script src="./assets/js/js/jquery.counterup.min.js"></script>
+<script src="./assets/js/js/waypoints.min.js"></script>
+<script src="./assets/js/js/jquery.countdown.min.js"></script>
+<script src="./assets/js/js/hover-direction-snake.min.js"></script>
+
+<!-- contact js -->
+<script src="./assets/js/js/contact.js"></script>
+<script src="./assets/js/js/jquery.form.js"></script>
+<script src="./assets/js/js/jquery.validate.min.js"></script>
+<script src="./assets/js/js/mail-script.js"></script>
+<script src="./assets/js/js/jquery.ajaxchimp.min.js"></script>
+
+<!-- Jquery Plugins, main Jquery -->	
+<script src="./assets/js/js/plugins.js"></script>
+<script src="./assets/js/js/main.js"></script>
+</body>
+</html>
 
