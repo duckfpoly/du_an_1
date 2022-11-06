5f65bb5aee7b07e9b0045555b4c58a5b17f11bdd
duckfpoly <thienduc.nguyen098@gmail.comm>
2022-11-06 22:16:27 +0700

    up

--- routes/route_site.php
+++ routes/route_site.php
@@ -1,38 +1,22 @@
 <?php
-    if(isset($_GET['v'])) {
-        $v = $_GET['v'];
-        switch ($v) {
-            case "lesson":
-                include_once 'views/site/lession.php';
-                break;
-            default:
-                location($host."page_not_found");
-                break;
-        }
+$v = isset($_GET['v']) ? $_GET['v'] : "";
+if($v){
+    switch ($v){
+        case "lessions":
+            include 'views/site/lessions/lessions.php';
+            break;
+        case "about":
+            include 'views/site/about/about.php';
+            break;
+        case "contact":
+            include 'views/site/contact/contact.php';
+            break;
+        default:
+            include 'views/site/home.php';
+            break;
     }
-    else {
-        include_once 'views/site/home.php';
-    }
-?>
-<?php
-        $v = isset($_GET['v']) ? $_GET['v'] : "";
-        if($v){
-            switch ($v){
-                case "lessions":
-                    include 'views/site/lessions/lessions.php';
-                    break;
-                case "about":
-                    include 'views/site/about/about.php';
-                    break; 
-                case "contact":
-                    include 'views/site/contact/contact.php';
-                    break;    
-                default:
-                    include 'views/site/home.php';
-                    break;
-            }
 
-        }else{
-            include_once 'views/site/home.php';
-        }
+}else{
+    include_once 'views/site/home.php';
+}
 ?>
