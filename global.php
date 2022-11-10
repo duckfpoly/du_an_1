552c7bcd145a1c35da7697098626281fd98fc032
duckfpoly <thienduc.nguyen098@gmail.comm>
2022-11-10 17:05:27 +0700

    up

--- global.php
+++ global.php
@@ -140,25 +140,25 @@
         }
     }
 
-    // function send_mail($mail,$output,$title){
-    //     $mailer         = new PHPMailer(true);
-    //     $mailer->SMTPDebug = 0;
-    //     $mailer->isSMTP();
-    //     $mailer->Host       = 'smtp.gmail.com';
-    //     $mailer->SMTPAuth   = true;
-    //     $mailer->Username   = 'ndcake.store@gmail.com';
-    //     $mailer->Password   = 'mswwgrjitnohamff';
-    //     $mailer->SMTPSecure = 'tls';
-    //     $mailer->Port       = 587;
-    //     $mailer->setFrom('ndcake.store@gmail.com', 'DDH Manager');
-    //     $mailer->addAddress($mail);
-    //     $mailer->isHTML(true);
-    //     $mailer->AddReplyTo('ndcake.store@gmail.com', 'DDH Manager');
-    //     $body = $output;
-    //     $mailer->Subject = 'DDH Manager - '.$title;
-    //     $mailer->Body = $body;
-    //     $mailer->send();
-    // }
+     function send_mail($mail,$output,$title){
+         $mailer         = new PHPMailer(true);
+         $mailer->SMTPDebug = 0;
+         $mailer->isSMTP();
+         $mailer->Host       = 'smtp.gmail.com';
+         $mailer->SMTPAuth   = true;
+         $mailer->Username   = 'ndcake.store@gmail.com';
+         $mailer->Password   = 'mswwgrjitnohamff';
+         $mailer->SMTPSecure = 'tls';
+         $mailer->Port       = 587;
+         $mailer->setFrom('ndcake.store@gmail.com', 'DDH Manager');
+         $mailer->addAddress($mail);
+         $mailer->isHTML(true);
+         $mailer->AddReplyTo('ndcake.store@gmail.com', 'DDH Manager');
+         $body = $output;
+         $mailer->Subject = 'DDH Manager - '.$title;
+         $mailer->Body = $body;
+         $mailer->send();
+     }
 
     function cut_email($email){
         $string = $email;
@@ -182,6 +182,59 @@
         }
     }
 
+
+    function pagination_normal($tbl,$limit_data){
+        $sql = "SELECT count(id) AS total FROM $tbl";
+        $row = query_one($sql);
+        $total_records = $row['total'];
+        $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+        $limit = $limit_data;
+        $total_page = ceil($total_records / $limit);
+        if ($current_page > $total_page) {
+            $current_page = $total_page;
+        } else if ($current_page < 1) {
+            $current_page = 1;
+        }
+        $start = ($current_page - 1) * $limit;
+        $data_pani = "SELECT * FROM $tbl LIMIT $start, $limit";
+        $row = query($data_pani);
+        $arr = [$row, $current_page, $total_page];
+        return $arr;
+    }
+
+    // current page: get page url
+    // total page: tổng số bản ghi của một table chia cho số bản ghi muốn hiện ra màn hình
+    function pagination($current_page, $total_page, $url){
+        if ($current_page > 1 && $total_page > 1) {
+            echo '<a class="" href="' . $url . '?page=' . ($current_page - 1) . '"><</a>';
+        }
+        for ($i = 1; $i <= $total_page; $i++) {
+            if ($i == $current_page) {
+                echo '<span class="active">' . $i . '</span> ';
+            } else {
+                echo '<a href="' . $url . '?page=' . $i . '">' . $i . '</a> ';
+            }
+        }
+        if ($current_page < $total_page && $total_page > 1) {
+            echo '<a href="' . $url . '?page=' . ($current_page + 1) . '">></a> ';
+        }
+    }
+
+    function check_time_end($date){
+        $time_end = strtotime ( '+6 month' , strtotime ( $date ) ) ;
+        $time_end = date ( 'Y-m-d' , $time_end );
+        return strtotime(date('Y-m-d')) == strtotime($time_end) ? "true" : "false";
+    }
+
+    function check_time_start($date){
+        $time_start =   strtotime ($date);
+        $time_now   =   strtotime(date('Y-m-d'));
+        return $time_start == $time_now ? "true" : "false";
+    }
+
+    function format_date($date){
+        return (new DateTimeImmutable($date))->format('d/m/Y');
+    }
 
 
 
