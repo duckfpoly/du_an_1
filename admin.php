3edcd1317e5eecca82023e64cfbf25736dbd2304
duckfpoly <thienduc.nguyen098@gmail.comm>
2022-11-10 16:35:14 +0700

    up

--- admin.php
+++ admin.php
@@ -45,30 +45,30 @@
             <?php include_once 'routes/route_admin.php'; ?>
             <?php include_once 'views/admin/layouts/footer.php'; ?>
         </main>
-        <div class="fixed-plugin">
-          <div class="card shadow-lg">
+    </div>
+    <div class="fixed-plugin">
+        <div class="card shadow-lg">
             <div class="card-header pb-0 pt-3">
-              <div class="float-start">
-                <h5 class="mt-3 mb-0">DDH Configurator</h5>
-                <p>Change Theme</p>
-              </div>
-              <div class="float-end mt-4">
-                <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-                  <i class="fa fa-close"></i>
-                </button>
-              </div>
-              <!-- End Toggle Button -->
+                <div class="float-start">
+                    <h5 class="mt-3 mb-0">DDH Configurator</h5>
+                    <p>Change Theme</p>
+                </div>
+                <div class="float-end mt-4">
+                    <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+                        <i class="fa fa-close"></i>
+                    </button>
+                </div>
+                <!-- End Toggle Button -->
             </div>
             <div class="card-body overflow-auto">
-              <hr class="horizontal dark ">
-              <div class="mb-5 d-flex">
-                <h6 class="mb-0">Light / Dark</h6>
-                <div class="form-check form-switch ps-0 ms-auto my-auto">
-                  <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+                <hr class="horizontal dark ">
+                <div class="mb-5 d-flex">
+                    <h6 class="mb-0">Light / Dark</h6>
+                    <div class="form-check form-switch ps-0 ms-auto my-auto">
+                        <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/venobox/2.0.4/venobox.min.js"></script>
--- views/admin/layouts/sidebar.php
+++ views/admin/layouts/sidebar.php
@@ -2,7 +2,7 @@
 <!-- <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 ps " id="sidenav-main"> -->
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="#" >
+        <a class="navbar-brand m-0" href="<?= DASHBOARD ?>" >
             <img src="<?= $host ?>/assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">DDH Manager</span>
         </a>
