12df8ca7aff6ceef0c5bd95038f9460b177cdd97
Nguyen Trung Duc <huyga612@gmail.com>
2022-11-27 17:23:50 +0700

    delete var_dump

--- controllers/site/mycourseController.php
+++ controllers/site/mycourseController.php
@@ -2,7 +2,6 @@
     $idUser = $_SESSION['user']['id'];
     $idCourse = $_GET['id'];
     $course = detail_mycourse($idUser, $idCourse);
-    var_dump($course);
     include 'views/site/mycourses/mycourse.php'
 
 ?>
