eada03db3f57be294d1a156c43dbd4fbdfb34531
Nguyen Trung Duc <huyga612@gmail.com>
2022-11-29 09:44:15 +0700

    add my course

--- controllers/site/mycourseController.php
+++ controllers/site/mycourseController.php
@@ -2,6 +2,6 @@
     $idUser = $_SESSION['user']['id'];
     $idCourse = $_GET['id'];
     $course = detail_mycourse($idUser, $idCourse);
-    include 'views/site/mycourses/mycourse.php'
+    include 'views/site/mycourses/mycourse.php';
 
 ?>
